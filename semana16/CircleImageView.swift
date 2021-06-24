b0992878667d7a7265af850f062593ffa8adf2a4
linder3hs <linderhassinger00@gmail.com>
2021-06-24 09:16:21 -0500

    first commit :zap:

---
+++ semana16/CircleImageView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleImageView.swift
+//  semana16
+//
+//  Created by Linder Hassinger on 24/06/21.
+//
+
+import SwiftUI
+
+struct CircleImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImageView()
+    }
+}
---
+++ semana16/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  semana16
+//
+//  Created by Linder Hassinger on 24/06/21.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
